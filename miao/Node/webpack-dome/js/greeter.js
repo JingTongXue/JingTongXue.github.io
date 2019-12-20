8fd4590bbfea6cfc749d320d7b67b96987d9e393
JingTongXue <985660403@qq.com>
2019-12-20 21:57:55 +0800

    delete webpack

--- miao/Node/webpack-dome/js/greeter.js
+++
@@ -1,4 +0,0 @@
-var domGreet = document.createElement("div");
-domGreet.textContent = "Welcome to learn Wbepack";
-var root = document.querySelector("#root");
-root.appendChild(domGreet);
--- miao/Node/webpack-dome/js/index.js
+++
@@ -1,8 +0,0 @@
-var domDiv=document.createElement("div");
-domDiv.textContent="Hello Webpack!"
-var root=document.querySelector("#root");
-root.appendChild(domDiv);
-
-var image = document.createElement("img");
-image.src = require('../image/5.jpg');
-root.appendChild(image);
